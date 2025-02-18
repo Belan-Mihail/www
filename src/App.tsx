f8f8651d9e6293e84f7788a88c3b36a55d9b4477
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-02-18 20:24:40 +0100

    edit navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   
@@ -6,7 +7,7 @@ function App() {
   return (
     
     <div>
-      <h1 className=' text-red-500 text-xl'>Hello</h1>
+      <Navbar />
     </div>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import { Link, useLocation } from 'react-router-dom';
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
-
-export default Navbar
+    const location = useLocation(); 
+  
+    return (
+      <nav className="bg-gray-900 text-white py-4">
+        <div className="container mx-auto flex flex-col items-center">
+          
+          <div className="text-3xl font-bold mb-4">
+            <span>Логотип</span>
+          </div>
+  
+          
+          <div className="flex space-x-8">
+            
+            {location.pathname === '/' ? (
+              
+              <>
+                <a href="#startseite" className="hover:text-yellow-500 transition duration-300">Startseite</a>
+                <a href="#unsere-arbeiten" className="hover:text-yellow-500 transition duration-300">Unsere Arbeiten</a>
+                <a href="#statistiken" className="hover:text-yellow-500 transition duration-300">Statistiken</a>
+                <a href="#bewertungen" className="hover:text-yellow-500 transition duration-300">Bewertungen</a>
+                <a href="#kontakte" className="hover:text-yellow-500 transition duration-300">Kontakte</a>
+              </>
+            ) : (
+              
+              <>
+                <Link to="/" className="hover:text-yellow-500 transition duration-300">Главная</Link>
+                <Link to="#unsere-arbeiten" className="hover:text-yellow-500 transition duration-300">Unsere Arbeiten</Link>
+                <Link to="#statistiken" className="hover:text-yellow-500 transition duration-300">Statistiken</Link>
+                <Link to="#bewertungen" className="hover:text-yellow-500 transition duration-300">Bewertungen</Link>
+                <Link to="#kontakte" className="hover:text-yellow-500 transition duration-300">Kontakte</Link>
+              </>
+            )}
+          </div>
+        </div>
+      </nav>
+    );
+  };
+  
+  export default Navbar;
